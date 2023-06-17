46ef4dba78aa816f1cba3f8db5256b31f38b014a
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-17 18:11:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	set age(age){
+		this.age= age;
+	}
+	get name(){
+		return this.name;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	contructor(name,age){
+		super(name,age);
+	}
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	contructor(name,age){
+		super(name,age);
+	}
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
